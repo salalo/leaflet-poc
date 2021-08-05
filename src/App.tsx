71a54ac2eb8852a677c85b9273c3f94067e3e03a
Wojciech Sala <wojtek@wojciechs-mbp.home>
2021-08-05 14:32:49 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function App() {
+  return (
+    <div className="App">
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="map"
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
