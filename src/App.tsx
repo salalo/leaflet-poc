c1792282ee7ff6951a695cfe2914c5f3a4f28438
Wojciech Sala <wojtek@wojciechs-mbp.home>
2021-08-05 17:23:41 +0200

    sdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "./App.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+
+const multiPolyline: LatLngExpression[][] = [
+  [
+    [51.5, -0.1],
+    [51.5, -0.12],
+    [51.52, -0.12],
+  ],
+  [
+    [51.5, -0.05],
+    [51.5, -0.06],
+    [51.52, -0.06],
+  ],
+];
 
 function App() {
   return (
@@ -8,13 +28,14 @@ function App() {
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className="map"
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Polyline pathOptions={{ color: "blue" }} positions={multiPolyline} />
         <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
