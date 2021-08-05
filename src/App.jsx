0e29649c58053d71a950c526c78b5615f0c7f4dd
w.sala <w.sala@lekseek.com>
2021-08-05 23:56:51 +0200

    react drawing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import React, { useRef, useState } from "react";
 import "leaflet";
 import "leaflet-editable";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  Polyline,
-} from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "./App.css";
-import { ReactLeafletEditable } from "react-leaflet-editable";
-// const ReactLeafletEditable = require("react-leaflet-editable");
+import ReactLeafletEditable from "react-leaflet-editable";
 
-const multiPolyline = [
-  [
-    [51.5, -0.1],
-    [51.5, -0.12],
-    [51.52, -0.12],
-  ],
-  [
-    [51.5, -0.05],
-    [51.5, -0.06],
-    [51.52, -0.06],
-  ],
-];
+// const multiPolyline = [
+//   [
+//     [51.5, -0.1],
+//     [51.5, -0.12],
+//     [51.52, -0.12],
+//   ],
+//   [
+//     [51.5, -0.05],
+//     [51.5, -0.06],
+//     [51.52, -0.06],
+//   ],
+// ];
 
 const App = () => {
   const editRef = useRef();
   const [map, setMap] = useState();
-  const editPolygon = () => {
-    editRef.current.startPolygon();
+  const editPolyline = () => {
+    editRef.current.startPolyline();
   };
   const whenMapCreated = (map) => {
     setMap(map);
@@ -37,6 +30,9 @@ const App = () => {
 
   return (
     <ReactLeafletEditable ref={editRef} map={map}>
+      <button onClick={editPolyline} className="editable-btn">
+        Start/Stop drawing
+      </button>
       <MapContainer
         whenCreated={whenMapCreated}
         editable={true}
@@ -45,19 +41,10 @@ const App = () => {
         scrollWheelZoom={true}
         className="map"
       >
-        <button onClick={editPolygon} className="editable-btn">
-          polygon
-        </button>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Polyline pathOptions={{ color: "blue" }} positions={multiPolyline} />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
       </MapContainer>
     </ReactLeafletEditable>
   );
